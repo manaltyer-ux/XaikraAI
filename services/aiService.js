7acf25aa1f76b3868fce808114720b544ecc82ac
manaltyer-ux <manaltyer@gmail.com>
2026-08-08 07:22:44 -1200

    aiService.js

---
+++ services/aiService.js
@@ -0,0 +1,27 @@
+(function () {
+  window.AIService = {
+    sendMessage: function (userText, imageDataUrl, requestId) {
+      const currentEmotions = window.EmotionService.getEmotions();
+      const userPersona = window.UserPersona.get();
+      const recentMessages = window.Conversation.getRecent(5);
+      const memoryText = window.MemoryService.getFormattedMemoryText();
+      const personality = window.aiPersonality || {};
+
+      const fullContext = window.ContextBuilder.buildAIContext({
+        currentMessage: userText,
+        recentMessages: recentMessages,
+        memoryText: memoryText,
+        emotions: currentEmotions,
+        aiPersonality: personality,
+        userPersona: userPersona
+      });
+
+      return window.ServerConnector.sendPromptPayload({
+        contextText: fullContext,
+        userText: userText,
+        imageDataUrl: imageDataUrl,
+        requestId: requestId
+      });
+    }
+  };
+})();
